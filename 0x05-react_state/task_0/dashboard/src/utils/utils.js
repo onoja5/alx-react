5471e4a2389a07b4f97cc3be93d27463c635341f
Michael Onoja <onoja16@gmail.com>
2023-08-11 15:08:17 +0100

    New files updated

--- 0x05-react_state/task_0/dashboard/src/utils/utils.js
+++
@@ -1,5 +0,0 @@
-export const getFullYear = () => new Date().getFullYear();
-
-export const getFooterCopy = (isIndex) => isIndex? 'Holberton school': 'Holberton School main dashboard';
-
-export const getLatestNotification = () => "<strong>Urgent requirement</strong> - complete by EOD";
